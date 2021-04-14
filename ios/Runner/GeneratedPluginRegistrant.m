792c4e7272f7a28926572fc80a4cce196988c972
Sailesh Dahal <saileshbro@gmail.com>
2021-04-14 20:41:15 +0545

    feat: add null-safety

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_twitter_login/TwitterLoginPlugin.h>)
+#import <flutter_twitter_login/TwitterLoginPlugin.h>
+#else
+@import flutter_twitter_login;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [TwitterLoginPlugin registerWithRegistrar:[registry registrarForPlugin:@"TwitterLoginPlugin"]];
+}
+
+@end
